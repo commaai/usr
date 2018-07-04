21195b0ebaea7100cb03ddef1d513b3c5264ffe5
George Hotz <george@comma.ai>
2018-07-04 12:07:14 -0700

    geos

---
+++ lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/data/data/com.termux/files/usr/lib"
+XSLT_LIBS="-lxslt -L/data/data/com.termux/files/usr/lib -lxml2 -lm "
+XSLT_INCLUDEDIR="-I/data/data/com.termux/files/usr/include"
+MODULE_VERSION="xslt-1.1.32"
